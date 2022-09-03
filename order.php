b01b4f702a4dd3b32bb0fff2adbb0493ad4cb8fa
nayemdeshipay <nayem@deshipay.com>
2022-09-03 14:19:06 +0600

    first commit

---
+++ order.php
@@ -0,0 +1,56 @@
+<?php include('header.php'); ?>
+<body>
+<?php include('navbar.php'); ?>
+<div class="container">
+	<h1 class="page-header text-center">ORDER LIST</h1>
+	<form method="POST" action="purchase.php">
+		<table class="table table-striped table-bordered">
+			<thead>
+				<th class="text-center"><input type="checkbox" id="checkAll"></th>
+				<th>Category</th>
+				<th>Product Name</th>
+				<th>Price</th>
+				<th>Quantity</th>
+			</thead>
+			<tbody>
+				<?php 
+					$sql="select * from product left join category on category.categoryid=product.categoryid order by product.categoryid asc, productname asc";
+					$query=$conn->query($sql);
+					// echo $query;
+					$iterate=0;
+					while($row=$query->fetch_array()){
+						?>
+						<tr>
+							<td class="text-center"><input type="checkbox" value="<?php echo $row['productid']; ?>||<?php echo $iterate; ?>" name="productid[]" style=""></td>
+							<td><?php echo $row['catname']; ?></td>
+							<td><?php echo $row['productname']; ?></td>
+							<td name = "price" class="text-right">&#x20A8; <?php echo number_format($row['price'], 2); ?></td>
+							<td><input type="text" class="form-control" name="quantity_<?php echo $iterate; ?>"></td>
+						</tr>
+						<?php
+						$iterate++;
+					}
+				?>
+			</tbody>
+		</table>
+		
+		<div class="row center">
+			<div class="col-md-3">
+				<input type="text" name="customer" class="form-control" placeholder="Customer Name" required>
+			</div>
+			<div class="col-md-2" style="margin-left:-20px;">
+				<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> Save</button>
+			</div>
+		</div>
+	</form>
+</div>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#checkAll").click(function(){
+	    	$('input:checkbox').not(this).prop('checked', this.checked);
+		});
+	});
+</script>
+<footer style="margin-bottom: 20px;"><center>&copy; 2022 All Rights Reserved By Ruhul</center></footer>
+</body>
+</html>
