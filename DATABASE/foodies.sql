3341755b90ec990a7ac3c1e6afb330809d56a3d5
EMNayon <emranmasudnayon.017@gmail.com>
2022-09-10 04:11:42 +0600

    some changed are  database file

--- DATABASE/foodies.sql
+++ DATABASE/foodies.sql
@@ -1,11 +1,7 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
 -- http://www.phpmyadmin.net
---
 -- Host: 127.0.0.1
--- Generation Time: Oct 09, 2018 at 05:18 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +13,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `foodsys`
+-- Database: `foodies`
 --
 
 -- --------------------------------------------------------
